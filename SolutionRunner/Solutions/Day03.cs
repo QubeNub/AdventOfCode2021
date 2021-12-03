990ab60cfb8898d2875feae79c3becba23745e4f
Joey Franken <joey.franken@gmail.com>
2021-12-03 12:30:49 +0100

    Day 3 part 1

---
+++ SolutionRunner/Solutions/Day03.cs
@@ -0,0 +1,50 @@
+﻿using Tidy.AdventOfCode;
+
+namespace TestRunner.Solutions;
+
+[Day(2021, 3)]
+public class Day03 : Day<string[]>
+{
+    public int ByteLength = 12;
+    
+    public override string[] ParseInput(string rawInput)
+    {
+        return rawInput.Split('\n').ToArray();
+    }
+
+    public override object ExecutePart1()
+    {
+        string gamma = "", epsilon = "";
+        var nrOfOnes = 0;
+        var nrOfZeroes = 0;
+        for (var o = 0; o < ByteLength; o++)
+        {
+            foreach (var b in Input)
+            {
+                nrOfOnes += b[o] == '1' ? 1 : 0;
+                nrOfZeroes += b[o] == '0' ? 1 : 0;
+            }
+
+            if (nrOfOnes > nrOfZeroes)
+            {
+                gamma += '1';
+                epsilon += '0';
+            }
+            else
+            {
+                gamma += '0';
+                epsilon += '1';
+            }
+            
+            nrOfOnes = 0;
+            nrOfZeroes = 0;
+        }
+        
+        return Convert.ToUInt16(gamma, 2) * Convert.ToUInt16(epsilon, 2);
+    }
+
+    public override object ExecutePart2()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ SolutionTests/Day03Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using TestRunner.Solutions;
+
+namespace SolutionTests;
+
+public class Day03Tests
+{
+    private Day03 _sut;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _sut = new Day03();
+        _sut.ByteLength = 5;
+        _sut.Input = new[]
+        {
+           "00100",
+           "11110",
+           "10110",
+           "10111",
+           "10101",
+           "01111",
+           "00111",
+           "11100",
+           "10000",
+           "11001",
+           "00010",
+           "01010"
+        };
+    }
+
+    [Test]
+    public void TestPart1()
+    {
+        var result = _sut.ExecutePart1();
+        Assert.AreEqual(198, result);
+    }
+
+    [Test]
+    public void TestPart2()
+    {
+        var result = _sut.ExecutePart2();
+        Assert.AreEqual(900, result);
+    }
+}
